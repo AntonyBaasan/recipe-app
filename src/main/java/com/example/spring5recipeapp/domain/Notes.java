e41f6750fdce7538331d45658e7a4ad985ebbc5c
Ankhbayar <antony.baasan@gmail.com>
2018-02-26 23:50:46 -0500

    add simple test

--- src/main/java/com/example/spring5recipeapp/domain/Notes.java
+++ src/main/java/com/example/spring5recipeapp/domain/Notes.java
@@ -14,27 +14,31 @@ public class Notes {
     @Lob
     private String recipeNotes;
 
+    public Notes() {
+    }
+
     public long getId() {
-        return id;
+        return this.id;
+    }
+
+    public Recipe getRecipe() {
+        return this.recipe;
+    }
+
+    public String getRecipeNotes() {
+        return this.recipeNotes;
     }
 
     public void setId(long id) {
         this.id = id;
     }
 
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
     }
 
     public void setRecipeNotes(String recipeNotes) {
         this.recipeNotes = recipeNotes;
     }
+
 }
